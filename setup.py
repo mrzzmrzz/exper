dbe7fe936bdcb775a3014784de898b0ba1ad72c7
Runze Ma <runze.ma@outlook.com>
2023-11-15 20:53:15 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+
+print(long_description)
+
+setup(
+    name='exper',
+    version='0.1.2',
+    author="Mrzz",
+
+    description="This is a python package for running deep learning experiments. "
+                "Users can rapidly run their experiments by importing this module.",
+    packages=find_packages(),
+    long_description=long_description,
+    install_requires=["torch", "torch_geometric"],
+    include_package_data=True
+)
