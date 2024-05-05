0fbfaa39065206315eb4e756b76284df801d8c2b
Runze Ma <runze.ma@outlook.com>
2024-05-05 17:48:54 +0800

    test gpg

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as f:
 
 
 setup(
-    name='exper',
-    version='0.2.0',
+    name="exper",
+    version="0.2.0",
     author="Mrzz",
     description="This is a python package for running deep learning experiments. "
-                "Users can rapidly run their experiments by importing this module.",
+    "Users can rapidly run their experiments by importing this module.",
     packages=find_packages(),
     long_description=long_description,
     install_requires=["torch", "torch_geometric"],
-    include_package_data=True
+    include_package_data=True,
 )
